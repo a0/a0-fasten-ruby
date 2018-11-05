e7cc1f1d7461dcb8d2e0a64599cd81237345fa10
Aldrin Martoq <a@a0.cl>
2018-11-05 01:07:30 -0300

    Nevermind…

--- spec/error_spec.rb
+++ spec/error_spec.rb
@@ -1,14 +1,12 @@
 RSpec.shared_examples 'error handling' do |use_threads|
   process_model = use_threads ? 'threads' : 'processes'
 
-  max = OS.windows? ? 10 : 100
-
   it "using #{process_model}, early stop in case of failure" do |ex|
     FileUtils.rm_rf Dir.glob('*.testfile')
 
     f = Fasten::Runner.new name: ex.description, workers: 1, worker_class: ErrorWorker, developer: false, use_threads: use_threads
 
-    max.times do |index|
+    100.times do |index|
       shell = OS.windows? ? "ruby -e 'sleep 0.05; require \"fileutils\"; FileUtils.touch \"#{index}.testfile\"'" : "sleep 0.05; touch #{index}.testfile"
       f.add Fasten::Task.new name: index.to_s, shell: shell
     end
@@ -25,7 +23,7 @@ RSpec.shared_examples 'error handling' do |use_threads|
 
     f = Fasten::Runner.new name: ex.description, workers: 10, worker_class: ErrorWorker, developer: false, use_threads: use_threads
 
-    max.times do |index|
+    100.times do |index|
       shell = OS.windows? ? "ruby -e 'sleep 0.3; require \"fileutils\"; FileUtils.touch \"#{index}.testfile\"'" : "sleep 0.3; touch #{index}.testfile"
       f.add Fasten::Task.new name: index.to_s, shell: shell
     end
