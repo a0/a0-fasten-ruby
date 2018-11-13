1c00b6e6a4c7c1944e7790678357d4a75797b9fd
Aldrin Martoq <a@a0.cl>
2018-11-13 08:48:58 -0300

    Making rubocop happier.

--- spec/std_spec.rb
+++ spec/std_spec.rb
@@ -5,13 +5,13 @@ RSpec.shared_examples 'stdout, stderr' do |use_threads|
     FileUtils.rm_rf 'fasten/log'
 
     f = Fasten::Runner.new name: ex.description, workers: 5, use_threads: use_threads
-    Array.new(25) { |index| "std-#{index}"}.each do |name|
+    Array.new(25) { |index| "std-#{index}" }.each do |name|
       f.add Fasten::Task.new name: name, shell: "echo SHELL STDOUT ES #{name}"
     end
 
     f.perform
 
-    Array.new(25) { |index| "std-#{index}"}.each do |name|
+    Array.new(25) { |index| "std-#{index}" }.each do |name|
       text = File.open("fasten/log/task/#{name}.log").grep(/SHELL STDOUT ES/).first.to_s.chomp
       expect(text).to eq("SHELL STDOUT ES #{name}")
     end
@@ -21,13 +21,13 @@ RSpec.shared_examples 'stdout, stderr' do |use_threads|
     FileUtils.rm_rf 'fasten/log'
 
     f = Fasten::Runner.new name: ex.description, workers: 5, use_threads: use_threads
-    Array.new(25) { |index| "std-#{index}"}.each do |name|
+    Array.new(25) { |index| "std-#{index}" }.each do |name|
       f.add Fasten::Task.new name: name, shell: "echo SHELL STDERR ES #{name}>&2"
     end
 
     f.perform
 
-    Array.new(25) { |index| "std-#{index}"}.each do |name|
+    Array.new(25) { |index| "std-#{index}" }.each do |name|
       text = File.open("fasten/log/task/#{name}.log").grep(/SHELL STDERR ES/).first.to_s.chomp
       expect(text).to eq("SHELL STDERR ES #{name}")
     end
@@ -37,13 +37,13 @@ RSpec.shared_examples 'stdout, stderr' do |use_threads|
     FileUtils.rm_rf 'fasten/log'
 
     f = Fasten::Runner.new name: ex.description, workers: 5, use_threads: use_threads
-    Array.new(25) { |index| "std-#{index}"}.each do |name|
+    Array.new(25) { |index| "std-#{index}" }.each do |name|
       f.add Fasten::Task.new name: name, ruby: "STDOUT.puts 'RUBY STDOUT ES #{name}.constant'; $stdout.puts 'RUBY stdout ES #{name}.object'"
     end
 
     f.perform
 
-    Array.new(25) { |index| "std-#{index}"}.each do |name|
+    Array.new(25) { |index| "std-#{index}" }.each do |name|
       text = File.open("fasten/log/task/#{name}.log").grep(/RUBY STDOUT ES/).first.to_s.chomp
       expect(text).to eq("RUBY STDOUT ES #{name}.constant")
       text = File.open("fasten/log/task/#{name}.log").grep(/RUBY stdout ES/).first.to_s.chomp
@@ -55,13 +55,13 @@ RSpec.shared_examples 'stdout, stderr' do |use_threads|
     FileUtils.rm_rf 'fasten/log'
 
     f = Fasten::Runner.new name: ex.description, workers: 5, use_threads: use_threads
-    Array.new(25) { |index| "std-#{index}"}.each do |name|
+    Array.new(25) { |index| "std-#{index}" }.each do |name|
       f.add Fasten::Task.new name: name, ruby: "STDERR.puts 'RUBY STDERR ES #{name}.constant'; $stderr.puts 'RUBY stderr ES #{name}.object'"
     end
 
     f.perform
 
-    Array.new(25) { |index| "std-#{index}"}.each do |name|
+    Array.new(25) { |index| "std-#{index}" }.each do |name|
       text = File.open("fasten/log/task/#{name}.log").grep(/RUBY STDERR ES/).first.to_s.chomp
       expect(text).to eq("RUBY STDERR ES #{name}.constant")
       text = File.open("fasten/log/task/#{name}.log").grep(/RUBY stderr ES/).first.to_s.chomp
